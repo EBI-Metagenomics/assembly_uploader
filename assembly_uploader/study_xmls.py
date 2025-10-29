b9b3a23a8fbf771ad73ce4f4f2f5e1b8bb1ada8e
Sofia Ochkalova <61541445+ochkalova@users.noreply.github.com>
2025-10-29 10:39:45 +0100

    remove argparse from imports

--- assembly_uploader/study_xmls.py
+++ assembly_uploader/study_xmls.py
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import argparse
 import hashlib
 import importlib.metadata
 import xml.dom.minidom as minidom
